572fbc7b13df640ec40ccf9c903c2ccfed6b1553
Gabriel <gabrielestebansalvatore@gmail.com>
2020-07-12 12:03:26 -0300

    Home controle finished - my contracts,  edit property

--- MyLeasing.Web/Data/Entities/Property.cs
+++ MyLeasing.Web/Data/Entities/Property.cs
@@ -50,6 +50,18 @@ namespace MyLeasing.Web.Data.Entities
 
         public ICollection<Contract> Contracts { get; set; }
 
-      
+        public string FirstImage
+        {
+            get
+            {
+                if (PropertyImages == null || PropertyImages.Count == 0)
+                {
+                    return "https://myleasing.azurewebsites.net/images/Properties/noImage.png";
+                }
+
+                return PropertyImages.FirstOrDefault().ImageUrl;
+            }
+        }
+
     }
 }
